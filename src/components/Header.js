7b46158856368cf69de2152fdb033c81eb9f5c4f
OliverosA <aldair_oliveros09@hotmail.es>
2022-12-22 01:47:04 -0600

    transition added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,22 @@
-import { Box, Container, Grid } from "@mui/material";
+import { Box, Container, Grid, Grow } from "@mui/material";
 const Header = ({ logo }) => {
   return (
-    <Container maxWidth='sm'>
-      <Grid display={"flex"} justifyContent={"center"} mb={3}>
-        <Box
-          sx={{
-            display: "flex",
-            margin: "auto",
-            height: "35vh",
-          }}
-        >
-          {" "}
-          <img src={logo} className='logoImage' alt='cakeImage' />
-        </Box>
-      </Grid>
-    </Container>
+    <Grow in={true} timeout={1000}>
+      <Container maxWidth='sm'>
+        <Grid display={"flex"} justifyContent={"center"} mb={3}>
+          <Box
+            sx={{
+              display: "flex",
+              margin: "auto",
+              height: "35vh",
+            }}
+          >
+            {" "}
+            <img src={logo} className='logoImage' alt='cakeImage' />
+          </Box>
+        </Grid>
+      </Container>
+    </Grow>
   );
 };
 
